29fa979e8c1148ea571802f6c07a81ddeea12028
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-08-21 09:37:31 +0900

    Update ch04-02-references-and-borrowing/src/main.rs

--- ch04-02-references-and-borrowing/src/main.rs
+++ ch04-02-references-and-borrowing/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    let s1 = String::from("hello");
+    let s = String::from("hello");
 
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    change(&s);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &String) {
+    some_string.push_str(", world");
 }
