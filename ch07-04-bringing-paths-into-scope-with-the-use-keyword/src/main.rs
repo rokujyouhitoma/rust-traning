5769dd86b796479e6353009b2db1e60ab061915e
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-08-28 09:37:52 +0900

    Update ch07-04-bringing-paths-into-scope-with-the-use-keyword

--- ch07-04-bringing-paths-into-scope-with-the-use-keyword/src/main.rs
+++ ch07-04-bringing-paths-into-scope-with-the-use-keyword/src/main.rs
@@ -1,5 +1,6 @@
-use rand::Rng;
+use std::collections::HashMap;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 }
