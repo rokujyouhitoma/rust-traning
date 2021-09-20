05c9aad6ff9a71506e12ce0c0853b444eb9b8f09
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-20 09:44:22 +0900

    Implement of main func by use of env::args()

--- ook/src/main.rs
+++ ook/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+fn entry_point(_filename: String) {
+    println!("{}", _filename);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("You must supply a filename");
+        std::process::exit(1);
+    }
+    entry_point(args[1].clone());
 }
