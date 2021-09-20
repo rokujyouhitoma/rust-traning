ab060d2c1aa897f7ddae29cda61118e351d963ad
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2021-09-20 10:05:27 +0900

    Implement of entry_point by use of File

--- ook/src/main.rs
+++ ook/src/main.rs
@@ -1,15 +1,27 @@
 use std::env;
+use std::fs::File;
 
-fn entry_point(args: Vec<String>) {
+fn run(_fp: File) {
+    // TODO
+}
+
+fn entry_point(args: Vec<String>) -> std::io::Result<()> {
     if args.len() < 2 {
         println!("You must supply a filename");
         std::process::exit(1);
     }
     let filename: &String = &args[1];
     println!("{}", filename);
+    let file = File::open(filename)?;
+    run(file);
+    std::process::exit(0);
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    entry_point(args);
+    let res = entry_point(args);
+    match res {
+        Err(e) => println!("{:?}", e),
+        _ => (),
+    }
 }
